243cf89feb33a223f7290fbb7df350989c0f01f5
hysimok <jeonghy95@gmail.com>
2020-11-28 23:24:57 +0900

    minor fix

--- ft_strlen.s
+++ ft_strlen.s
@@ -3,10 +3,10 @@ section.text:
 
 _ft_strlen:
 	mov rax, 0x0 ; count set to 0 - rax is returned value
-	_start_loop:
+	start_loop:
 		cmp byte [rdi + rax], 0x0 ; compare rdi + rax char to \0
-		jz _end_loop ; if compare true then go to flag _end_loop
+		jz end_loop ; if compare true then go to flag end_loop
 		inc rax	; increment count
-		jmp _start_loop ; restart loop
-	_end_loop:
+		jmp start_loop ; restart loop
+	end_loop:
 	ret
