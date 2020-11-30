dbc4147f79fc41e6eef56ffc908ba72bd22e6326
hysimok <jeonghy95@gmail.com>
2020-11-30 16:03:28 +0900

    adjust return value

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -2,20 +2,42 @@ section .text
 	global _ft_strcmp
 
 _ft_strcmp:							; rdi = s1, rsi = s2
-		push	rcx
-		mov		rcx, -1
+	push	rcx
+	mov		rcx, -1
 
-start_loop:	
+	start_loop:	
 		inc		rcx
 		mov		al, BYTE [rdi+rcx]	; to store 1byte(8bits)
 		mov		bl, BYTE [rsi+rcx]
-		cmp		ax, 0
-		je		end
-		cmp		bl, 0
-		je		end
-		cmp		al, bl			; if al == bl
-    	je		start_loop			; then loop
+		cmp		al, 0x0
+		je		al_null
+		cmp		bl, 0x0
+		je		bl_null
+		cmp		al, bl				; if al == bl
+		je		start_loop			; then loop
+		ja		above
+		jb		below
 		jmp		end
+
+al_null:
+	cmp		bl, 0x0
+	jz		end
+	jmp		below
+
+bl_null:
+	cmp		al, 0x0
+	jz		end
+	jmp		above
+
+above:
+	pop		rcx
+	mov		rax, 1
+	ret
+
+below:
+	pop 	rcx
+	mov		rax, -1
+	ret
 
 end:
 	pop		rcx
--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hjung <hjung@student.42seoul.kr>           +#+  +:+       +#+        */
+/*   By: hjung <hjung@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/27 19:21:35 by hjung             #+#    #+#             */
-/*   Updated: 2020/11/29 19:57:49 by hjung            ###   ########.fr       */
+/*   Updated: 2020/11/30 16:02:21 by hjung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,27 @@ int main()
     printf("length : %d\n", (int)ft_strlen("Pizza"));
 
     printf("result of strcmp : %d\n", strcmp("\0", "\200"));
-    printf("result of strcmp : %d\n", ft_strcmp("\0", "\200"));
+    printf("result of ft_strcmp : %d\n", ft_strcmp("\0", "\200"));
+    printf("---------------------------\n");
+
+    printf("result of strcmp : %d\n", strcmp("\200", "\0"));
+    printf("result of ft_strcmp : %d\n", ft_strcmp("\200", "\0"));
+    printf("---------------------------\n");
+
+    printf("result of strcmp : %d\n", strcmp("aa", "aa"));
+    printf("result of ft_strcmp : %d\n", ft_strcmp("aa", "aa"));
+    printf("---------------------------\n");
 
     printf("result of strcmp : %d\n", strcmp("j", "a"));
-    printf("result of strcmp : %d\n", ft_strcmp("\200", "a"));
+    printf("result of ft_strcmp : %d\n", ft_strcmp("j", "a"));
+    printf("---------------------------\n");
+    
+    printf("result of strcmp : %d\n", strcmp("a", "j"));
+    printf("result of ft_strcmp : %d\n", ft_strcmp("a", "j"));
+    printf("---------------------------\n");
+
+    printf("result of strcmp : %d\n", strcmp("a\200\0", "abcd"));
+    printf("result of ft_strcmp : %d\n", ft_strcmp("a\200\0", "abcd"));
+    printf("---------------------------\n");
     return (0);
 }
