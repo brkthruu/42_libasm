031bf38194cba6b53a0b6540691941d3d6ea2882
hysimok <jeonghy95@gmail.com>
2020-12-01 15:14:14 +0900

    refactore ft_strcmp

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -10,24 +10,20 @@ _ft_strcmp:							; rdi = s1, rsi = s2
 		mov		al, BYTE [rdi+rcx]	; to store 1byte(8bits)
 		mov		bl, BYTE [rsi+rcx]
 		cmp		al, 0x0
-		je		al_null
+		je		ret_null
 		cmp		bl, 0x0
-		je		bl_null
+		je		ret_null
 		cmp		al, bl				; if al == bl
 		je		start_loop			; then loop
 		ja		above
 		jb		below
 		jmp		end
 
-al_null:
-	cmp		bl, 0x0
-	jz		end
+ret_null:
+	cmp		al, bl
+	ja		above
+	je		end
 	jmp		below
-
-bl_null:
-	cmp		al, 0x0
-	jz		end
-	jmp		above
 
 above:
 	pop		rcx
